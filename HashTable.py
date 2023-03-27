a41b0af4161fa148081ebe0845662e416701de0f
v20035017 <camille.vedani@outlook.fr>
2023-03-27 13:19:55 +0200

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   HashTable.py
    
    Changes not staged for commit:
    	modified:   __pycache__/HashTable.cpython-310.pyc
    
    Untracked files:
    	LinkedList.py
    	__pycache__/LinkedList.cpython-310.pyc

--- HashTable.py
+++ HashTable.py
@@ -1,23 +1,33 @@
 import math
+import LinkedList
 class HashTable:
     """
     HashTable is a class that implements a hash table with it s own hach function "getHash".
-    HasTable : key() --> Value  
+    HasTable : key() --> Value  using Chain class to define the pair(key,value)
+    we intilize thee hashtable as empty and we can insert ,find , remove element from our hashTable
+
+
 
     
     
     
     """
+
 
     def __init__(self,sizeMax=89) :
         self.sizeMax=sizeMax
         self.size=0
-        self.values=[None]*sizeMax
+        self.keys=[None]*sizeMax
 
         
             
     
     def getHash(self,key):
+        """
+        returns: the index of the key in the hashTable
+
+        param : key:"set of charecters" 
+        """
         K=0
         for charcter in set(key):
             K+=ord(charcter)
@@ -25,14 +35,33 @@ class HashTable:
         return K%self.sizeMax
         
     
-    def __getitem__(self,index):
-         hash= self.getHash(index)
-         return self[hash]
+    def __getitem__(self,key):
+         """
+         returns : the linked list associated with our index 
+         param : key:"set of charecters" 
+         """
+         hash= self.getHash(key)
+         linkedList = self.keys[hash]
+         #we go throug our lonked list until we find the value associated with the hash
+         while linkedList is not None and linkedList.key != hash:
+            linkedList = linkedList.next
+                
+            if linkedList is None:
+                    #not found 
+                    return None
+            else:
+                    #found
+                    return linkedList.value
+            
+
 
     def __setitem__(self,key,value):
+
         self.size+=1
         hash = self.getHash(key)
         charSet=self.wordToCharSet(value)
+
+
         if str(charSet) not in self[hash]:
             self[key][str(charSet)]=[value]
         if value not in self[key][str(charSet)]:
